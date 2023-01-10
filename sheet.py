2d941b2dadc60ae921c61ca01f5243198b52f085
hhuston <90809677+hhuston@users.noreply.github.com>
2023-01-10 14:50:33 -0500

    Update notecard for non-disprovers

--- sheet.py
+++ sheet.py
@@ -3,9 +3,16 @@ class Sheet():
     def __init__(self, p):
         global notecard
         global players
+        players = p
         notecard = [['Col. Mustard\t', 'Prof. Plum\t','Mr. Green\t', 'Mrs. Peacock\t', 'Miss Scarlet\t', 'Mrs. White\t', 
                    'Knife\t\t', 'Candlestick\t', 'Revolver\t', 'Rope\t\t', 'Lead Pipe\t', 'Wrench\t', 
-                   'Hall\t\t', 'Lounge\t', 'Dining Room\t', 'Kitchen\t', 'Ballroom\t', 'Conservatory\t', 'Billiard Room\t', 'Library\t', 'Study\t\t']] + ([[' '] * 21] * num_players)
+                   'Hall\t\t', 'Lounge\t', 'Dining Room\t', 'Kitchen\t', 'Ballroom\t', 'Conservatory\t', 'Billiard Room\t', 'Library\t', 'Study\t\t']] + [[' '] * 21 for _ in range(len(players))]
+        print('Enter the number of your cards and the communal cards, separated by spaces: ')
+        for i in range(1, 22):
+            print('\n\t', i, ' - ', notecard[0][i - 1])
+        user_cards = input('Enter your cards: ').split(' ')
+        
+        print(self)
     
     def __str__(self):
         s = '=' * 18 + '=' * len(notecard) * 3
@@ -20,24 +27,56 @@ class Sheet():
 
     def move(self):
         print('For each prompt, please enter the number that corresponds to the suggestion\n')
-        print('Who is being suggested?', '\n\t', 1, ' - ', notecard[0][0], '\n\t', 2, ' - ', notecard[0][1], '\n\t', 3, ' - ', notecard[0][2],
-            '\n\t', 4, ' - ', notecard[0][3], '\n\t', 5, ' - ', notecard[0][4], '\n\t', 6, ' - ', notecard[0][5])
-        person = (int(input('Enter a number: ')) - 1)
+        print('Who is being suggested?')
+        for i in range(1, 7):
+            print('\n\t', i, ' - ', notecard[0][i - 1])
+        p = (int(input('Enter a number: ')) - 1)
 
-        print('\n\nWhat weapon is being suggested?', '\n\t', 1, ' - ', notecard[0][6], '\n\t', 2, ' - ', notecard[0][7], '\n\t', 3, ' - ', notecard[0][8],
-            '\n\t', 4, ' - ', notecard[0][9], '\n\t', 5, ' - ', notecard[0][10], '\n\t', 6, ' - ', notecard[0][11])
-        weapon = (int(input('Enter a number: ')) + 5)
+        print('\n\nWhat weapon is being suggested?')
+        for i in range(1, 7):
+            print('\n\t', i, ' - ', notecard[0][i + 5])
+        w = (int(input('Enter a number: ')) + 5)
 
-        print('\n\nWhat room is being suggested?', '\n\t', 1, ' - ', notecard[0][12], '\n\t', 2, ' - ', notecard[0][13], '\n\t', 3, ' - ', notecard[0][14],
-            '\n\t', 4, ' - ', notecard[0][15], '\n\t', 5, ' - ', notecard[0][16], '\n\t', 6, ' - ', notecard[0][17], '\n\t', 7, ' - ', notecard[0][18],
-            '\n\t', 8, ' - ', notecard[0][19], '\n\t', 9, ' - ', notecard[0][20])
-        room = (int(input('Enter a number: ')) + 11)
+        print('\n\nWhat room is being suggested?')
+        for i in range(1, 10):
+            print('\n\t', i, ' - ', notecard[0][i + 11])
+        r = (int(input('Enter a number: ')) + 11)
+
+        print('\n\nWho made the move?')
+        for i in range(1, len(players) + 1):
+            print('\n\t', i, ' - ', players[i - 1])
+        s = (int(input('Enter a number: ')) - 1)
+
+        print('\n\nWho disproved the move?')
+        for i in range(1, len(players) + 1):
+            print('\n\t', i, ' - ', players[i - 1])
+        print('\n\t', len(players) + 1, ' - No one')
+        d = (int(input('Enter a number: ')) - 1)
+
+        players_skipped = []
+        if d != len(players):
+            i = s + 1
+            while True:
+                if i == len(players):
+                    i = 0
+                if i == d:
+                    break
+                players_skipped += [i]
+                i += 1
+        else:
+            players_skipped = [x for x in list(range(len(players))) if x != s]
+        for i in players_skipped:
+            print(notecard[i+1][p])
+            notecard[i+1][p] = '-'
+            notecard[i+1][w] = '-'
+            notecard[i+1][r] = '-'
         
 
 
 def test_sheet():
     s = Sheet(['Chris', 'Winnie', 'Harrison'])
-    # s.move()
+    print(s)
+    s.move()
     print(s)
 
 if __name__ == '__main__':
