b7ff7d37ebabee1b284481fe4d9f1e56aedb373b
hhuston <90809677+hhuston@users.noreply.github.com>
2023-01-11 19:57:28 -0500

    Game File
    Move some functions and tasks into a separate game.py file to improve code readability and to loop through moves until the game ends

---
+++ game.py
@@ -0,0 +1,21 @@
+import sheet
+
+class ClueGame():
+    
+    def start_game(self):
+        game_sheet = sheet.NoteSheet(self.get_players())
+        print('\n')
+        game_sheet.move(game_sheet.get_move_input())
+
+    def get_players(self):
+        while True:
+            names = input('Enter each player\'s name in turn order with your name at the end, separated by spaces:\n')
+            names = names.strip().split(' ')
+            if len(names) <= 1:
+                print('Too few players')
+            elif len(names) >= 7:
+                print('Too many players')
+            else:
+                return names
+    
+    
--- sheet.py
+++ sheet.py
@@ -3,10 +3,8 @@ class Sheet():
     def __init__(self, p):
         global notecard
         global players
-        global players_count
         global trackers
         players = p
-        players_count = [[0] * len(players)]
         trackers = [[0] * 24 for _ in range(len(players) + 1)]
         notecard = [['Col. Mustard\t', 'Prof. Plum\t','Mr. Green\t', 'Mrs. Peacock\t', 'Miss Scarlet\t', 'Mrs. White\t', 
                    'Knife\t\t', 'Candlestick\t', 'Revolver\t', 'Rope\t\t', 'Lead Pipe\t', 'Wrench\t', 
@@ -25,6 +23,139 @@ class Sheet():
             else:
                 notecard[len(notecard) - 1][i] = '-'
 
+    def set_card_owner(self, person, card):
+        notecard[person][card] = 'X'
+        trackers[person][22] += 1
+        for i in range(1, len(notecard) - 1):
+            if i != person:
+                notecard[i][card] = '-'
+                trackers[i][23] += 1
+
+    def get_move_input(self):
+        prompt = 'Enter a number: '
+        print('For each prompt, please enter the number that corresponds to the suggestion\n')
+        
+        print('Who made the suggestion?')
+        for i in range(1, len(players) + 1):
+            print('\n\t', i, ' - ', players[i - 1])
+        suggestor = (int(input(prompt)) - 1)
+        
+        print('\n\nWhat person is being suggested?')
+        for i in range(1, 7):
+            print('\n\t', i, ' - ', notecard[0][i - 1])
+        person = (int(input(prompt)) - 1)
+
+        print('\n\nWhat weapon is being suggested?')
+        for i in range(1, 7):
+            print('\n\t', i, ' - ', notecard[0][i + 5])
+        weapon = (int(input(prompt)) + 5)
+
+        print('\n\nWhat room is being suggested?')
+        for i in range(1, 10):
+            print('\n\t', i, ' - ', notecard[0][i + 11])
+        room = (int(input(prompt)) + 11)
+
+        print('\n\nWho disproved the suggestion?')
+        for i in range(1, len(players) + 1):
+            print('\n\t', i, ' - ', players[i - 1])
+        print('\n\t', len(players) + 1, ' -  No one')
+        disprover = (int(input(prompt)) - 1)
+        return suggestor, person, weapon, room, disprover
+
+    def move(self, s, p, w, r, d):
+        global players_skipped
+        players_skipped = []
+        if d != len(players):
+            self.record_player_disprove(s, p, w, r, d)
+        else:
+            players_skipped = [x for x in list(range(len(players))) if x != s]
+        for i in players_skipped:
+            print(notecard[i+1][p])
+            for j in [p, w, r]:
+                if notecard[i+1][j] != '-':
+                    notecard[i+1][j]= '-'
+                    trackers[i+1][23] += 1
+
+    def record_player_disprove(self, s, p, w, r, d):
+        global players_skipped
+        i = s + 1
+        while True:
+            if i == len(players):
+                i = 0
+            if i == d:
+                break
+            players_skipped += [i]
+            i += 1
+        d += 1
+        if notecard[d][p] == '-' and notecard[d][w] == '-':
+            self.set_card_owner(d, r)
+        elif notecard[d][p] == '-' and notecard[d][r] == '-':
+            self.set_card_owner(d, w)
+        elif notecard[d][w] == '-' and notecard[d][r] == '-':
+            self.set_card_owner(d, p)
+        elif s != len(players) - 1:
+            trackers[d][21] = trackers[d][21] + 1 if trackers[d][21] != 0 else 3
+            count = trackers[d][21]
+            for j in [p, w, r]:
+                if notecard[d][j] != '-':
+                    notecard[d][j] = '*'
+                    trackers[d][j] += count
+        else:
+            print('\n\nWhat card did', players[d - 1], 'show you?\n\t1 - ', notecard[0][p])
+            print('\t2 - ', notecard[0][w], '\n\t3 - ', notecard[0][r])
+            shown_card = int(input('Enter a number: '))
+            match shown_card:
+                case 1:
+                    shown_card = p
+                case 2:
+                    shown_card = w
+                case 3:
+                    shown_card = r
+            self.set_card_owner(d, shown_card)
+
+    def check_possibile_cards(self, player):
+        # for i in range(1, len(players)):
+        if trackers[player][22] == 3 and trackers[player][23] != 18:
+            notecard[player] = list(map(lambda x: '-' if x == ' ' else 'X', notecard[player]))
+            return
+        if trackers[player][23] == 18 and trackers[player][22] != 3:
+            for j in range(21):
+                if notecard[player][j] == ' ':
+                    self.set_card_owner(player, j)
+            return
+        t3, t4, t5 = self.get_possible_cards(player)
+        for j in [3, 4, 5, 4, 3]:
+            if j == 3 and len(t3) == 1:
+                self.one_in_tracker(player, t3, t4, t5)
+            elif j == 4 and len(t4) == 1:
+                self.one_in_tracker(player, t4, t5, t3)
+            elif j == 5 and len(t5) == 1:
+                self.one_in_tracker(player, t5, t3, t4)
+        all_trackers = t3 + t4 + t5
+        if len(all_trackers) == 9:
+            for j in range(21):
+                if j not in all_trackers:
+                    notecard[player][j] = '-'
+                    trackers[player][23] += 1
+
+    def get_possible_cards(self, player):
+        tracker_3, tracker_4, tracker_5 = [], [], []
+        for j in range(21):
+            if trackers[player][j] in [3, 7, 8, 12] and notecard[player][j] != '-':
+                tracker_3 += [j]
+            if trackers[player][j] in [4, 7, 9, 12] and notecard[player][j] != '-':
+                tracker_4 += [j]
+            if trackers[player][j] in [5, 8, 9, 12] and notecard[player][j] != '-':
+                tracker_5 += [j]
+        return tracker_3, tracker_4, tracker_5
+
+    def one_in_tracker(self, i, a, b, c):
+        self.set_card_owner(i, a[0])
+        if a[0] in b:
+            b.remove(a[0])
+        if a[0] in c:
+            c.remove(a[0])
+
     def __str__(self):
         s = '\n\n\t\t | ' + ' | '.join([name[0] for name in players]) + ' |\n'
         s += '=' * 18 + '=' * len(notecard) * 3
@@ -36,177 +167,3 @@ class Sheet():
                 s += '\n' + '=' * 18 + '=' * len(notecard) * 3
         s += '\n' + '=' * 18 + '=' * len(notecard) * 3
         return s
-
-    def move(self):
-        prompt = 'Enter a number: '
-        print('For each prompt, please enter the number that corresponds to the suggestion\n')
-        
-        print('Who made the suggestion?')
-        for i in range(1, len(players) + 1):
-            print('\n\t', i, ' - ', players[i - 1])
-        s = (int(input(prompt)) - 1)
-        
-        print('\n\nWho is being suggested?')
-        for i in range(1, 7):
-            print('\n\t', i, ' - ', notecard[0][i - 1])
-        p = (int(input(prompt)) - 1)
-
-        print('\n\nWhat weapon is being suggested?')
-        for i in range(1, 7):
-            print('\n\t', i, ' - ', notecard[0][i + 5])
-        w = (int(input(prompt)) + 5)
-
-        print('\n\nWhat room is being suggested?')
-        for i in range(1, 10):
-            print('\n\t', i, ' - ', notecard[0][i + 11])
-        r = (int(input(prompt)) + 11)
-
-        print('\n\nWho disproved the suggestion?')
-        for i in range(1, len(players) + 1):
-            print('\n\t', i, ' - ', players[i - 1])
-        print('\n\t', len(players) + 1, ' -  No one')
-        d = (int(input(prompt)) - 1)
-
-        players_skipped = []
-        if d != len(players):
-            i = s + 1
-            while True:
-                if i == len(players):
-                    i = 0
-                if i == d:
-                    break
-                players_skipped += [i]
-                i += 1
-            d += 1
-            if notecard[d][p] == '-' and notecard[d][w] == '-':
-                notecard[d][r] = 'X'
-                trackers[d][22] += 1
-                for i in range(1, len(notecard) - 1):
-                    if i != d:
-                        notecard[i][r] = '-'
-                        trackers[i][23] += 1
-            elif notecard[d][p] == '-' and notecard[d][r] == '-':
-                notecard[d][w] = 'X'
-                trackers[d][22] += 1
-                for i in range(1, len(notecard) - 1):
-                    if i != d:
-                        notecard[i][w] = '-'
-                        trackers[i][23] += 1
-            elif notecard[d][w] == '-' and notecard[d][r] == '-':
-                notecard[d][p] = 'X'
-                trackers[d][22] += 1
-                for i in range(1, len(notecard) - 1):
-                    if i != d:
-                        notecard[i][p] = '-'
-                        trackers[i][23] += 1
-            elif s != len(players) - 1:
-                trackers[d][21] = trackers[d][21] + 1 if trackers[d][21] != 0 else 3
-                count = trackers[d][21]
-
-                if notecard[d][p] != '-':
-                    notecard[d][p] = '*'
-                    trackers[d][p] += count
-                if notecard[d][w] != '-':
-                    notecard[d][w] = '*'
-                    trackers[d][w] += count
-                if notecard[d][r] != '-':
-                    notecard[d][r] = '*'
-                    trackers[d][r] += count
-            else:
-                print('\n\nWhat card did ', players[d], ' show you?\n\t1 - ', notecard[0][p], '\n\t2 - ', notecard[0][w], '\n\t3 - ', notecard[0][r])
-                shown_card = int(input(prompt))
-                shown_card = p if shown_card == 1 else w if shown_card == 2 else w
-                notecard[d + 1][w] = 'X'
-                trackers[d][22] += 1
-                for i in range(1, len(notecard) - 1):
-                    if i != d:
-                        notecard[i][w] = '-'
-                        trackers[i][23] += 1
-        else:
-            players_skipped = [x for x in list(range(len(players))) if x != s]
-        for i in players_skipped:
-            print(notecard[i+1][p])
-            if notecard[i+1][p] != '-':
-                notecard[i+1][p]= '-'
-                trackers[i+1][23] += 1
-            if notecard[i+1][w] != '-':
-                notecard[i+1][w] = '-'
-                trackers[i+1][23] += 1
-            if notecard[i+1][r] != '-':
-                notecard[i+1][r] = '-'
-                trackers[i+1][23] += 1
-
-        for i in range(1, len(players)):
-            if trackers[i][22] == 3 and trackers[i][23] != 18:
-                notecard[i] = list(map(lambda x: '-' if x == ' ' else 'X', notecard[i]))
-            elif trackers[i][23] == 18 and trackers[i][22] != 3:
-                for j in range(21):
-                    if notecard[i][j] == ' ':
-                        notecard[i][j] = 'X'
-                        trackers[i][22] += 1
-                        for k in range(1, len(notecard) - 1):
-                            if k != i:
-                                notecard[k][j] = '-'
-                                trackers[k][23] += 1
-            else:
-                tracker_3 = []
-                tracker_4 = []
-                tracker_5 = []
-                for j in range(21):
-                    if trackers[i][j] in [3, 7, 8, 12] and notecard[i][j] != '-':
-                        tracker_3 += [j]
-                    if trackers[i][j] in [4, 7, 9, 12] and notecard[i][j] != '-':
-                        tracker_4 += [j]
-                    if trackers[i][j] in [5, 8, 9, 12] and notecard[i][j] != '-':
-                        tracker_5 += [j]
-                for j in [3, 4, 5, 4, 3]:
-                    if j == 3 and len(tracker_3) == 1:
-                        notecard[i][tracker_3[0]] = 'X'
-                        trackers[i][22] += 1
-                        for k in range(1, len(notecard) - 1):
-                            if k != i:
-                                notecard[k][j] = '-'
-                                trackers[k][23] += 1
-                        if tracker_3[0] in tracker_4:
-                            tracker_4.remove(tracker_3[0])
-                        if tracker_3[0] in tracker_5:
-                            tracker_5.remove(tracker_3[0])
-                    elif j == 4 and len(tracker_4) == 1:
-                        notecard[i][tracker_4[0]] = 'X'
-                        trackers[i][22] += 1
-                        for k in range(1, len(notecard) - 1):
-                            if k != i:
-                                notecard[k][j] = '-'
-                                trackers[k][23] += 1
-                        if tracker_4[0] in tracker_3:
-                            tracker_3.remove(tracker_4[0])
-                        if tracker_4[0] in tracker_5:
-                            tracker_5.remove(tracker_4[0])
-                    elif j == 5 and len(tracker_5) == 1:
-                        notecard[i][tracker_5[0]] = 'X'
-                        trackers[i][22] += 1
-                        for k in range(1, len(notecard) - 1):
-                            if k != i:
-                                notecard[k][j] = '-'
-                                trackers[k][23] += 1
-                        if tracker_5[0] in tracker_4:
-                            tracker_4.remove(tracker_5[0])
-                        if tracker_5[0] in tracker_3:
-                            tracker_3.remove(tracker_5[0])
-                all_tracker = tracker_3 + tracker_4 + tracker_5
-                if len(all_tracker) == 9:
-                    for j in range(21):
-                        if j not in all_tracker:
-                            notecard[i][j] = '-'
-                            trackers[i][23] += 1
-
-
-def test_sheet():
-    s = Sheet(['Chris', 'Winnie', 'Harrison'])
-    print(s)
-    s.move()
-    print(s)
-
-if __name__ == '__main__':
-    test_sheet()
-
