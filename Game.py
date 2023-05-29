ed39c6b747a1228c821bfc92042e88cacaa40e66
hhuston <hhuston@stevens.edu>
2023-05-28 20:53:00 -0400

    Writing First Methods for Game

--- Game.py
+++ Game.py
@@ -1,9 +1,53 @@
-import Player
+from Player import Player
 
 class Game:
-    global Players
-    global cards
+
+    def __init__(self):
+        global Players
+        global cards
+
+        Players = []
+        cards = ['','Col. Mustard\t', 'Prof. Plum\t','Mr. Green\t', 'Mrs. Peacock\t', 'Miss Scarlet\t', 'Mrs. White\t', 
+                   'Knife\t\t', 'Candlestick\t', 'Revolver\t', 'Rope\t\t', 'Lead Pipe\t', 'Wrench\t', 
+                   'Hall\t\t', 'Lounge\t', 'Dining Room\t', 'Kitchen\t', 'Ballroom\t', 'Conservatory\t', 'Billiard Room\t', 'Library\t', 'Study\t\t']
+    
+    def get_players(self) -> None:
+        global Players
+        global num_players
+
+        names = input("Enter the names of players in order, with yourself last, separated by spaces:\n\t")
+        print()
+        names = names.strip().split(' ')
+        # Add some error checking eventually
+        num_players = len(names)
+        for i in range(num_players):
+            Players += [Player(names[i])]
+    
+    def set_user_cards(self) -> None:
+        global CARDS_PER_HAND
+
+        print(self.print_cards())
+        user_cards = input('Enter the numbers of your cards, separated by sapces:\n\t')
+        print()
+        user_cards = list(map(lambda x: int(x), user_cards.strip().split(' ')))
+        CARDS_PER_HAND = len(user_cards)
+        for card in user_cards:
+            Player[-1].set_owned(card)
+            for p in Players[:-1]:
+                p.set_unowned(card)
+
+    def print_cards(self) -> str:
+        return ("1 - " + cards[1] + ''   + "7  - " + cards[7]  + ''   + "13 - " + cards[13] + '\n' +
+                "2 - " + cards[2] + '\t' + "8  - " + cards[8]  + ''   + "14 - " + cards[14] + '\n' +
+                "3 - " + cards[3] + '\t' + "9  - " + cards[9]  + '\t' + "15 - " + cards[15] + '\n' +
+                "4 - " + cards[4] + ''   + "10 - " + cards[10] + ''   + "16 - " + cards[16] + '\n' +
+                "5 - " + cards[5] + ''   + "11 - " + cards[11] + '\t' + "17 - " + cards[17] + '\n' +
+                "6 - " + cards[6] + '\t' + "12 - " + cards[12] + '\t' + "18 - " + cards[18] + '\n' +
+                '\t' * 6 + "19 - " + cards[19] + '\n' +
+                '\t' * 6 + "20 - " + cards[20] + '\n' +
+                '\t' * 6 + "21 - " + cards[21] + '\n')
 
 if __name__ == "__main__":
-    # Stuff
-    pass
+    G = Game()
+    G.get_players()
+    G.set_user_cards()
--- Player.py
+++ Player.py
@@ -1,2 +1,54 @@
 class Player:
-    pass
+
+    def __init__(self, n : str) -> None:
+        global name
+        global owned_cards
+        global unowned_cards
+        global cards
+
+        self.name = n
+        self.owned_cards = 0
+        self.unowned_cards = 0
+        self.cards = [0 for x in range(21)]
+
+    def set_unonwed(self, card_number : int) -> None:
+        self.cards[card_number] = -1
+        self.unowned_cards += 1
+    
+    def set_owned(self, card_number : int) -> None:
+        self.cards[card_number] = 1
+        self.owned_cards += 1
+
+    def get_card_state(self, card_number: int) -> int:
+        return self.cards[card_number]
+
+    def analyze_hand(self, CARDS_PER_HAND : int, sidon_sum : int) -> list[int]:
+        new_owned = []
+
+        # See if all owned or unowned cards are accounted for
+        if owned_cards == CARDS_PER_HAND:
+            for i in range(21):
+                if cards[i] != 1:
+                    self.set_unowned(i)
+        elif unowned_cards == 21 - CARDS_PER_HAND:
+            for i in range(21):
+                if cards[i] != -1:
+                    self.set_owned(i)
+                    new_owned += [i]        
+        else:
+            for i in range(21):
+                if cards[i] == sidon_sum:
+                    self.set_owned(i)
+                    new_owned += [i]
+        return new_owned
+    
+    def output(self) -> list[str]:
+        def to_printable(x : int) -> str:
+            if x == -1:
+                return "-"
+            if x == 0:
+                return " "
+            if x == 1:
+                return "X"
+            return "*"
+        return [self.name[0]] + [to_printable(x) for x in cards]
