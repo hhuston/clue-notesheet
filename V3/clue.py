b093344f9bba05bc16c3fed3d5b056296a29d80f
hhuston <hhuston@stevens.edu>
2023-08-30 12:53:35 -0400

    Get guesses and disprovers done, working on algorithmic checking

--- V3/clue.py
+++ V3/clue.py
@@ -7,14 +7,16 @@ CARDS_PER_HAND = 0
 CARDS = []
 CARD_OPTIONS = ""
 
-box_fillers = {-1: " ", 0 : "-", 1 : "X"}
+box_fillers = {0: " ", -1 : "-", 1 : "X"}
 
 def set_unowned(player, card):
-    table[player][card] = 0
+    table[player][card] = -1
+    table[player][23] += 1
 def set_owned(player, card):
-    for i in range(NUM_PLAYERS):
-        set_unowned(player, card)
+    for others in range(1, NUM_PLAYERS + 1):
+        set_unowned(others, card)
     table[player][card] = 1
+    table[player][22] += 1
 
 def print_table():
     notecard = "\n\n\n" + '=' * 18 + '=' * NUM_PLAYERS * 4 + "\n"
@@ -62,7 +64,7 @@ if communal_cards != "":
     user_cards += [int(x.strip()) for x in (communal_cards.strip()).split(" ")]
 
 # Build the table
-table = [["\t\t"] + CARDS] + [[players[x][0]] + [-1 for x in range (21)] + [0,0,0] for x in range(NUM_PLAYERS)]
+table = [["\t\t"] + CARDS] + [[players[x][0]] + [0 for x in range (21)] + [0,0,2] for x in range(NUM_PLAYERS)]
 
 for card in range(1, 22):
     if card in user_cards:
@@ -73,7 +75,104 @@ for card in range(1, 22):
 # SIDON_SUM = pow(2, CARDS_PER_HAND + 1) - 2
 # sidon_lists = []
 
-for card in user_cards:
-    set_owned(USER_INDEX, card)    
+for card in range(1,22):
+    if card in user_cards:
+        set_owned(USER_INDEX, card)  
+    else:
+        set_unowned(USER_INDEX, card)  
 
-print_table()
+print("\n")
+def print_players():
+    for i in range(NUM_PLAYERS):
+        print("\t" + str(i + 1) + " - " + players[i])
+
+print_players()
+guessing_player = int(input("\nEnter the number of the player who is going first:\n\t").strip())
+eliminated_players = []
+print("\n\n------------------------------------------------------------------------------------\n\n")
+
+# Gameplay loop
+while True:
+    print_table()
+
+    print("It is " + players[guessing_player - 1] + "'s turn to guess\n\n")
+    print(CARD_OPTIONS)
+    guessed_cards = input("Enter the cards that " + players[guessing_player - 1] + " guessed, separated by spaces:\n\t")
+    guessed_cards = [int(card.strip()) for card in guessed_cards.strip().split(" ")]
+
+    print()
+    print_players()
+    print("\t" + str(NUM_PLAYERS + 1) + " - No disprover\n")
+    disprover = int(input("Enter the number of the person who disproved the guess:\n\t").strip())
+
+    # If the player makes an accusation then the game either ends or they are removed from the guessing rotation
+    if input("Is " + players[guessing_player - 1] + " making and accusation? (Y/N): \n\t").strip().upper() == "Y":
+        if input("Was " + players[guessing_player - 1] + "'s accusation correct? (Y/N): \n\t").strip().upper() == "Y":
+            print(players[guessing_player - 1] + " wins!")
+            break
+        else:
+            eliminated_players += [guessing_player]
+
+    # If no one could disprove the guess, set all the other players to not own any of the cards
+    if disprover == NUM_PLAYERS + 1:
+        for player in range(1, NUM_PLAYERS + 1):
+            if player != disprover and player != USER_INDEX:
+                for card in guessed_cards:
+                    set_unowned(player, card)
+    # If someone did disprove then set the people before the disprove as unowned for those cards
+    else:
+        temp = guessing_player
+        while temp != disprover:
+            if temp != USER_INDEX and temp != guessing_player:
+                for card in guessed_cards:
+                    set_unowned(temp, card)
+            temp += 1
+            if temp == NUM_PLAYERS:
+                temp = 1
+    
+        # If you were the guesser you know what the shown card is
+        if guessing_player == USER_INDEX and disprover != NUM_PLAYERS + 1:
+            print(CARD_OPTIONS)
+            set_owned(disprover, int(input("Enter the number of the card that you were shown:\n\t").strip()))
+    
+        # If you weren't the guesser and the disprover does not have one of the cards
+        # then assign a number to each of the possible cards that were shown
+
+        # When two of the cards guessed are known to be unowned by the disprover, they must own the third card
+        if -2 == table[disprover][guessed_cards[0]] + table[disprover][guessed_cards[1]] + table[disprover][guessed_cards[2]]:
+            for card in guessed_cards:
+                if table[disprover][card] == 0:
+                    set_owned(disprover, card)
+        # If the disprover is not known to own any of the cards then add the next disproval level from the sidon set to their value
+        elif table[disprover][guessed_cards[0]] != 1 and table[disprover][guessed_cards[1]] != 1 and table[disprover][guessed_cards[2]] != 1:
+            for card in guessed_cards:
+                if table[disprover][card] != -1:
+                    table[disprover][card] += sidon_set[table[disprover][24]]
+                    table[disprover][24] += 1
+    
+    # Start checking for deductions
+    board_changed = [True] * NUM_PLAYERS + 1
+    def board_was_changed(player):
+        for i in range(1, NUM_PLAYERS + 1):
+            if i != player:
+                board_changed[i] = True
+    
+    checking_player = 1
+    while True in board_changed:
+        board_changed[checking_player] = False
+        if table[checking_player][22] + table[checking_player][23] == 21:
+            continue
+        # Sidon set stuff
+
+        # If all of the owned or unowned cards have been found, fill in the rest
+        
+        if table[checking_player][22] == 3:
+            for card in range(1,22):
+                if table[checking_player][card] == 0:
+                    set_unowned(checking_player, card)
+        elif table[checking_player][23] == 18:
+            board_was_changed(checking_player)
+            for card in range(1,22):
+                if table[checking_player][card] == 0:
+                    set_owned(checking_player, card)
+
